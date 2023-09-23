409df7b5ab593d552ce4f0f01284fe1ec3362d02
Thomas Dankwah <dankwahthom@gmail.com>
2023-09-23 03:00:20 +0100

    sixth update

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -6,10 +6,10 @@ type Props = {};
 export default function BackgroundCircles({}: Props) {
   return (
     <div className="relative flex bottom-20 justify-center items-center">
-      <div className="bg-[#7DE5ED] mt-52  absolute border-[#7DE5ED] w-[450px] h-[450px] rounded-full animate-pulse" />
-      <div className="bg-[#81C6E8] mt-52  absolute border-[#81C6E8] w-[400px] h-[400px] rounded-full animate-pulse" />
-      <div className="bg-[#5DA7DB] mt-52  absolute border-[#5DA7DB] w-[350px] h-[350px] rounded-full animate-pulse" />
-      <div className="bg-[#5837D0] mt-52 absolute border-[#5837D0] w-[300px] h-[300px] rounded-full animate-pulse" />
+      <div className="bg-[#7DE5ED]  border-[#7DE5ED] w-[450px] h-[450px] backgroundCircle" />
+      <div className="bg-[#81C6E8]  border-[#81C6E8] w-[400px] h-[400px] backgroundCircle" />
+      <div className="bg-[#5DA7DB]  border-[#5DA7DB] w-[350px] h-[350px] backgroundCircle" />
+      <div className="bg-[#5837D0] border-[#5837D0] w-[300px] h-[300px] backgroundCircle" />
       <div className="bg-[#5837D0] mt-52 absolute border-[#5837D0] w-[200px] h-[200px] rounded-full animate-ping" />
     </div>
   );
